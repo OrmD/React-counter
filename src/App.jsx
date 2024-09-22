a6f5054d5aa57d961b31f0a4656c6eb2da6e9213
Рома Кушнірчук <groma1535@gmail.com>
2024-09-22 16:19:22 +0300

    Довавив текст в перший заголовок

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Counter</h1>
       <h2>Привіт це перша спроба</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
